fc58ff710ed83db34d95acc95feac426ff7c2abb
Stephen Ostapenko <stepanos2002@gmail.com>
2022-10-28 09:40:18 +0300

    done hw2

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -26,16 +26,16 @@ from _camtrack import (
 )
 
 
-def triangulate_points_on_two_frames(frame1: int, frame2: int, \
+def triangulate_points_on_two_frames(frame_1: int, frame_2: int, \
                                      corner_storage: CornerStorage, \
                                      view_mats: List[np.ndarray], \
                                      intrinsic_mat: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
 
-    correspondences = build_correspondences(corner_storage[frame1], corner_storage[frame2])
+    correspondences = build_correspondences(corner_storage[frame_1], corner_storage[frame_2])
     points3d, ids, _ = triangulate_correspondences(
         correspondences,
-        view_mats[frame1],
-        view_mats[frame2],
+        view_mats[frame_1],
+        view_mats[frame_2],
         intrinsic_mat,
         TriangulationParameters(
             max_reprojection_error = 5.0,
@@ -44,15 +44,172 @@ def triangulate_points_on_two_frames(frame1: int, frame2: int, \
         )
     )
 
-    return points3d, ids
+    return ids, points3d
+
+
+def get_best_points_for_frame(frame: int, frame_1: int, frame_2: int ,\
+                              corner_storage: CornerStorage, \
+                              view_mats: List[np.ndarray], \
+                              intrinsic_mat: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    
+    ids_1, points3d_1 = triangulate_points_on_two_frames(frame, frame_1, \
+                                                         corner_storage, \
+                                                         view_mats, \
+                                                         intrinsic_mat)
+    ids_2, points3d_2 = triangulate_points_on_two_frames(frame, frame_2, \
+                                                         corner_storage, \
+                                                         view_mats, \
+                                                         intrinsic_mat)
+    if (len(ids_1) < len(ids_2)):
+        ids_1, ids_2 = ids_2, ids_1
+        points3d_1, points3d_2 = points3d_2, points3d_1
+
+    return ids_1, points3d_1
+
+
+def calc_camera_positions(iteration: int, \
+                          frame_1: int, frame_2: int, frame_count: int, \
+                          corner_storage: CornerStorage, \
+                          rotations: List[np.ndarray], translations: List[np.ndarray], \
+                          view_mats: List[np.ndarray], \
+                          intrinsic_mat: np.ndarray, \
+                          point_cloud_builder: PointCloudBuilder, \
+                          ransac_parameters: dict, \
+                          shrink_outliers: bool) -> None:
+
+    for frame in range(frame_1 + 1, frame_2):
+        print(f"Processing frame #{frame}")
+
+        _, point_builder_ind, corners_ind = np.intersect1d(
+            point_cloud_builder.ids,
+            corner_storage[frame].ids,
+            return_indices = True
+        )
+
+        real_points = point_cloud_builder.points[point_builder_ind]
+        proj_points = corner_storage[frame].points[corners_ind]
+        assert(len(real_points) == len(proj_points))
+
+        retval, rvec, tvec, inliers = cv2.solvePnPRansac(
+            objectPoints = real_points,
+            imagePoints = proj_points,
+            **ransac_parameters
+        )
+
+        print(f"Found {len(inliers)} inliers")
+
+        outliers = np.setdiff1d(np.arange(0, len(real_points)), inliers.T, assume_unique = True)
+        point_cloud_builder.remove_points(outliers)
+
+        rotations[frame] = rvec
+        translations[frame] = tvec
+        view_mats[frame] = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
+
+        ids, points3d = get_best_points_for_frame(frame, frame_1, frame_2, corner_storage, view_mats, intrinsic_mat)
+        point_cloud_builder.add_points(ids, points3d, recalc_positions = False)
+
+        print(f"Triangulated {len(ids)} points")
+        print(f"Point cloud size is {len(point_cloud_builder.ids)}")
+
+
+    for frame in [*range(frame_1 - 1, -1, -1), *range(frame_2 + 1, frame_count)]:
+        if (not (view_mats[frame] is None)):
+            continue
+
+        print(f"Processing frame #{frame}")
+
+        _, point_builder_ind, corners_ind = np.intersect1d(
+            point_cloud_builder.ids,
+            corner_storage[frame].ids,
+            return_indices = True
+        )
+
+        real_points = point_cloud_builder.points[point_builder_ind]
+        proj_points = corner_storage[frame].points[corners_ind]
+        assert(len(real_points) == len(proj_points))
+
+        retval, rvec, tvec, inliers = cv2.solvePnPRansac(
+            objectPoints = real_points,
+            imagePoints = proj_points,
+            **ransac_parameters
+        )
+
+        print(f"Found {len(inliers)} inliers")
+
+        outliers = np.setdiff1d(np.arange(0, len(real_points)), inliers.T, assume_unique = True)
+        point_cloud_builder.remove_points(outliers)
+
+        rotations[frame] = rvec
+        translations[frame] = tvec
+        view_mats[frame] = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
+
+        ids, points3d = get_best_points_for_frame(frame, frame_1, frame_2, corner_storage, view_mats, intrinsic_mat)
+        point_cloud_builder.add_points(ids, points3d, recalc_positions = False)
+
+        print(f"Triangulated {len(ids)} points")
+        print(f"Point cloud size is {len(point_cloud_builder.ids)}")
+
+
+    if (not shrink_outliers):
+        return
+
+    print("\nShrinking outliers")
+
+    for frame in range(frame_count):
+        print(f"Processing frame #{frame}")
+
+        _, point_builder_ind, corners_ind = np.intersect1d(
+            point_cloud_builder.ids,
+            corner_storage[frame].ids,
+            return_indices = True
+        )
+
+        real_points = point_cloud_builder.points[point_builder_ind]
+        proj_points = corner_storage[frame].points[corners_ind]
+        assert(len(real_points) == len(proj_points))
+
+        retval, rvec, tvec, inliers = cv2.solvePnPRansac(
+            objectPoints = real_points,
+            imagePoints = proj_points,
+            **ransac_parameters
+        )
+
+        outliers = np.setdiff1d(np.arange(0, len(real_points)), inliers.T, assume_unique = True)
+        point_cloud_builder.remove_points(outliers)
+
+
+    print("\nRefining positions")
+
+    for frame in range(frame_count):
+        print(f"Processing frame #{frame}")
+
+        _, point_builder_ind, corners_ind = np.intersect1d(
+            point_cloud_builder.ids,
+            corner_storage[frame].ids,
+            return_indices = True
+        )
+
+        real_points = point_cloud_builder.points[point_builder_ind]
+        proj_points = corner_storage[frame].points[corners_ind]
+        assert(len(real_points) == len(proj_points))
+
+        retval, rvec, tvec, inliers = cv2.solvePnPRansac(
+            objectPoints = real_points,
+            imagePoints = proj_points,
+            **ransac_parameters
+        )
+
+        rotations[frame] = rvec
+        translations[frame] = tvec
+        view_mats[frame] = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
 
 
 def track_and_calc_colors(camera_parameters: CameraParameters,
                           corner_storage: CornerStorage,
                           frame_sequence_path: str,
                           known_view_1: Optional[Tuple[int, Pose]] = None,
-                          known_view_2: Optional[Tuple[int, Pose]] = None) \
-        -> Tuple[List[Pose], PointCloud]:
+                          known_view_2: Optional[Tuple[int, Pose]] = None) -> Tuple[List[Pose], PointCloud]:
+
     if known_view_1 is None or known_view_2 is None:
         raise NotImplementedError()
 
@@ -66,108 +223,55 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
     )
 
     frame_count = len(corner_storage)
-    view_mats = [None] * frame_count
-    view_mats[known_view_1[0]] = pose_to_view_mat3x4(known_view_1[1])
-    view_mats[known_view_2[0]] = pose_to_view_mat3x4(known_view_2[1])
+    frame_1 = known_view_1[0]
+    frame_2 = known_view_2[0]
 
+    rotations = [None] * frame_count
+    translations = [None] * frame_count
+    view_mats = [None] * frame_count
     point_cloud_builder = PointCloudBuilder()
-    points3d, ids = triangulate_points_on_two_frames(known_view_1[0], known_view_2[0], \
+
+    view_mat_1 = pose_to_view_mat3x4(known_view_1[1])
+    rotations[frame_1] = cv2.Rodrigues(view_mat_1[:, : 3])[0]
+    translations[frame_1] = np.array(view_mat_1[:, 3 :])
+    view_mats[frame_1] = view_mat_1
+
+    view_mat_2 = pose_to_view_mat3x4(known_view_2[1])
+    rotations[frame_2] = cv2.Rodrigues(view_mat_2[:, : 3])[0]
+    translations[frame_2] = np.array(view_mat_2[:, 3 :])
+    view_mats[frame_2] = view_mat_2
+
+    print("Building init point cloud\n")
+
+    ids, points3d = triangulate_points_on_two_frames(frame_1, frame_2, \
                                                      corner_storage, \
                                                      view_mats, \
-                                                     intrinsic_mat)
+                                                     intrinsic_mat
+    )
     point_cloud_builder.add_points(ids, points3d)
 
-    RANSAC_PARAMETERS = dict(
-        cameraMatrix = intrinsic_mat,
-        reprojectionError = 5.0,
-        confidence = 1 - 1e-6,
-        iterationsCount = 1000,
-        distCoeffs = None
-    )
-
-    for frame in range(known_view_1[0] + 1, known_view_2[0]):
-        _, point_builder_ind, corners_ind = np.intersect1d(
-            point_cloud_builder.ids,
-            corner_storage[frame].ids,
-            return_indices = True
-        )
-
-        real_points = point_cloud_builder.points[point_builder_ind]
-        proj_points = corner_storage[frame].points[corners_ind]
-        assert(len(real_points) == len(proj_points))
-
-        retval, rvec, tvec, inliers = cv2.solvePnPRansac(
-            objectPoints = real_points,
-            imagePoints = proj_points,
-            **RANSAC_PARAMETERS
-        )
-
-        outliers = np.setdiff1d(np.arange(0, len(real_points)), inliers.T, assume_unique = True)
-        point_cloud_builder.remove_points(outliers)
-
-        view_mats[frame] = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
-
-        points3d_1, ids_1 = triangulate_points_on_two_frames(known_view_1[0], frame, \
-                                                             corner_storage, \
-                                                             view_mats, \
-                                                             intrinsic_mat)
-        points3d_2, ids_2 = triangulate_points_on_two_frames(frame, known_view_2[0], \
-                                                             corner_storage, \
-                                                             view_mats, \
-                                                             intrinsic_mat)
-        if (len(ids_1) < len(ids_2)):
-            ids_1, ids_2 = ids_2, ids_1
-            points3d_1, points3d_2 = points3d_2, points3d_1
-
-        point_cloud_builder.add_points(ids_1, points3d_1, recalc_positions = False)
-
-    for frame in range(frame_count):
-        if (not (view_mats[frame] is None)):
-            continue
-
-        print("kek", frame)
-
-        _, point_builder_ind, corners_ind = np.intersect1d(
-            point_cloud_builder.ids,
-            corner_storage[frame].ids,
-            return_indices = True
-        )
-
-        real_points = point_cloud_builder.points[point_builder_ind]
-        proj_points = corner_storage[frame].points[corners_ind]
-        assert(len(real_points) == len(proj_points))
-
-        retval, rvec, tvec, inliers = cv2.solvePnPRansac(
-            objectPoints = real_points,
-            imagePoints = proj_points,
-            **RANSAC_PARAMETERS
-        )
-
-        outliers = np.setdiff1d(np.arange(0, len(real_points)), inliers.T, assume_unique = True)
-        print(outliers)
-        point_cloud_builder.remove_points(outliers)
-
-        '''_, point_builder_ind, corners_ind = np.intersect1d(
-            point_cloud_builder.ids,
-            corner_storage[frame].ids,
-            return_indices = True
-        )
-
-        rvec, tvec = cv2.solvePnPRefineLM(
-            objectPoints = real_points,
-            imagePoints = proj_points,
+    def RANSAC_PARAMETERS(reprojection_error):
+        return dict(
             cameraMatrix = intrinsic_mat,
-            distCoeffs = None,
-            rvec = rvec,
-            tvec = tvec
-        )'''
+            reprojectionError = reprojection_error,
+            confidence = 1 - 1e-6,
+            iterationsCount = 1000,
+            distCoeffs = None
+        )
 
-        view_mats[frame] = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
+    for iteration in range(1, 4):
+        calc_camera_positions(iteration, \
+                              frame_1, frame_2, frame_count, \
+                              corner_storage, \
+                              rotations, translations, \
+                              view_mats, \
+                              intrinsic_mat, \
+                              point_cloud_builder, \
+                              RANSAC_PARAMETERS(reprojection_error = ([0, 5, 10, 48])[iteration]), \
+                              shrink_outliers = (iteration == 3)
+        )
 
-        points3d, ids = triangulate_points_on_two_frames(known_view_2[0], frame, corner_storage, view_mats, intrinsic_mat)
-        point_cloud_builder.add_points(ids, points3d, recalc_positions = False)
-
-    print("my job here is done")
+    print("Camera positions calculated")
 
     calc_point_cloud_colors(
         point_cloud_builder,
